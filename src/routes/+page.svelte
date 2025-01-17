becee79c97d76808df72f4c42de79179201cd3e3
Uciel Sola <solauciel@gmail.com>
2025-01-17 20:06:18 -0300

    Refactor +page.svelte: Enhance UI and structure
    - Added a header to introduce the state management concept.
    - Grouped action buttons within a styled div for better layout.
    - Included a link to the GitHub repository for code reference.
    - Maintained existing functionality while improving user interface and accessibility.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,22 @@
   import { aStore, bStore, aActions, bActions, DispatcherDebug } from "$lib";
 </script>
 
-<button on:click={aActions.increment}>Increment A</button>
-<button on:click={aActions.decrement}>Decrement A</button>
+<h1>State Management with Svelte Runes, adapting some ideas from the Flux pattern</h1>
 
-<button on:click={bActions.increment}>Increment B</button>
-<button on:click={bActions.decrement}>Decrement B</button>
-<button on:click={bActions.reset}>Reset B</button>
+<a href="https://github.com/ucielsola/svelte-5-state-management">
+  🧑🏼‍💻 Check the code...
+</a>
 
-<button on:click={() => aActions.hydrate(10)}>Hydrate</button>
+<div class="buttons">
+  <button on:click={aActions.increment}>Increment A</button>
+  <button on:click={aActions.decrement}>Decrement A</button>
+
+  <button on:click={bActions.increment}>Increment B</button>
+  <button on:click={bActions.decrement}>Decrement B</button>
+  <button on:click={bActions.reset}>Reset B</button>
+  <button on:click={() => aActions.hydrate(10)}>Hydrate</button>
+</div>
+
 
 <p>Loaded: {aStore.loaded ? "Yes" : "No"}</p>
 
@@ -20,3 +28,19 @@
 <p>Mixed (derived a + b): {aStore.mixed}</p>
 
 <DispatcherDebug />
+
+<style>
+  * {
+    font-family: sans-serif;
+  }
+
+  .buttons {
+    display: flex;
+    gap: 0.5rem;
+    padding-block: 1rem;
+  }
+
+  a {
+    font-size: 1.25rem;
+  }
+</style>
